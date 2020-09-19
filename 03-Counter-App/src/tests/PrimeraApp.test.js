a948ee137c328cfd46f48c632fd1d88b8ae5076b
BrandonU <brandon.vargas1095@hotmail.com>
2020-09-19 03:53:56 -0500

    Enzime - Testing Unit Video 59

--- 03-Counter-App/src/tests/PrimeraApp.test.js
+++ 03-Counter-App/src/tests/PrimeraApp.test.js
@@ -1,13 +1,21 @@
-import { render } from '@testing-library/react';
-import PrimeraApp from '../PrimeraApp';
 import React from 'react';
+import { shallow } from 'enzyme';
 import '@testing-library/jest-dom';
 
-describe('Pruebas en PrimeraApp', () => {
-	test('Debe mostrar mensaje de saludo', () => {
-		const saludo = 'Hola soy Goku';
-		const { getByText } = render(<PrimeraApp saludo={saludo} />);
+import PrimeraApp from '../PrimeraApp';
 
-		expect(getByText(saludo)).toBeInTheDocument();
+describe('Pruebas en PrimeraApp', () => {
+	// test('Debe mostrar mensaje de saludo', () => {
+	// 	const saludo = 'Hola soy Goku';
+	// 	const { getByText } = render(<PrimeraApp saludo={saludo} />);
+
+	// 	expect(getByText(saludo)).toBeInTheDocument();
+	// });
+
+	test('Debe de mostrar PrimeraApp correctamente', () => {
+		const saludo = 'Hola Soy Goku';
+		const wrapper = shallow(<PrimeraApp saludo={saludo} />);
+
+		expect(wrapper).toMatchSnapshot();
 	});
 });
