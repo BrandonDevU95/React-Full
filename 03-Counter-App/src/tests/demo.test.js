5e9bdb20f56d7e8bab990244352f87031145e4e5
BrandonU <brandon.vargas1095@hotmail.com>
2020-09-19 01:04:18 -0500

    Using Jest - Expect - toBe Video 51

--- 03-Counter-App/src/tests/demo.test.js
+++ 03-Counter-App/src/tests/demo.test.js
@@ -1,3 +1,13 @@
-test('should be true', () => {
-	const isAvtive = true;
+// test('should be true', () => {
+// 	const isAvtive = true;
+// });
+
+describe('Pruebas en el archivo demo ', () => {
+	test('Deben ser iguales los strings', () => {
+		const mensaje = 'Hola Mundo';
+
+		const mensaje2 = `Hola Mundo`;
+
+		expect(mensaje).toBe(mensaje2);
+	});
 });
