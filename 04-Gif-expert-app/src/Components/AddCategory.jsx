3d7d0d16cc4ef5c5375ba02ead7d36e90421c195
BrandonU <brandon.vargas1095@hotmail.com>
2020-09-21 00:19:41 -0500

    Helpers - getGifts Video 77

--- 04-Gif-expert-app/src/Components/AddCategory.jsx
+++ 04-Gif-expert-app/src/Components/AddCategory.jsx
@@ -10,7 +10,7 @@ export const AddCategory = ({ setCategories }) => {
 	const handleSubmit = (e) => {
 		e.preventDefault();
 		if (inputValue.trim().length > 2) {
-			setCategories((cats) => [...cats, inputValue]);
+			setCategories((cats) => [inputValue, ...cats]);
 			setinputValue('');
 		}
 	};
--- 04-Gif-expert-app/src/Components/GifGrid.jsx
+++ 04-Gif-expert-app/src/Components/GifGrid.jsx
@@ -1,27 +1,12 @@
 import React, { useState, useEffect } from 'react';
+import { getGifs } from '../helpers/getGifs';
 import { GifGridItem } from './GifGridItem';
 
 export const GifGrid = ({ category }) => {
 	const [images, setImages] = useState([]);
 	useEffect(() => {
-		getGifs();
-	}, []);
-
-	const getGifs = async () => {
-		const url =
-			'https://api.giphy.com/v1/gifs/search?q=Rick+and+Morty&limit=10&api_key=ffAgHCnT8J2neqevUwAeMCdaqajcP2ze';
-		const resp = await fetch(url);
-		const { data } = await resp.json();
-		const gifs = data.map((img) => {
-			return {
-				id: img.id,
-				title: img.title,
-				url: img.images?.downsized_medium.url,
-			};
-		});
-		console.log(gifs);
-		setImages(gifs);
-	};
+		getGifs(category).then((imgs) => setImages(imgs));
+	}, [category]);
 
 	return (
 		<>
