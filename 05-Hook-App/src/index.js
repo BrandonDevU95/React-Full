ba7c49a1365d256518ab78dd79b5c73ed477a4f2
BrandonDevU95 <brandon.vargas1095@hotmail.com>
2020-10-08 13:59:40 -0500

    useFetch - CustomHook Video 111

--- 05-Hook-App/src/index.js
+++ 05-Hook-App/src/index.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { FormWithCustomHook } from './Components/02-useEffect/FormWithCustomHook';
+import { MultipleCustomHooks } from './Components/03-examples/MultipleCustomHooks';
 // import { HooksApp } from './HooksApp';
 // import { CounterApp } from './Components/01-useState/CounterApp';
 // import { SimpleForm } from './Components/02-useEffect/SimpleForm';
+// import { FormWithCustomHook } from './Components/02-useEffect/FormWithCustomHook';
 // import { CounterWithCustomHook } from './Components/01-useState/CounterWithCustomHook';
 
-ReactDOM.render(<FormWithCustomHook />, document.getElementById('root'));
+ReactDOM.render(<MultipleCustomHooks />, document.getElementById('root'));
