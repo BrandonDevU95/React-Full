7beb07436f01bc628fd9d119f7cd9936fd5a483c
BrandonDevU95 <brandon.vargas1095@hotmail.com>
2020-10-08 22:21:00 -0500

    Creating the shell from the TODOs list Video 126

--- 05-Hook-App/src/Components/08-useReducer/TodoApp.jsx
+++ 05-Hook-App/src/Components/08-useReducer/TodoApp.jsx
@@ -13,17 +13,45 @@ export const TodoApp = () => {
 		},
 	];
 
-	const [todo] = useReducer(todoReducer, initialState);
+	const [todos] = useReducer(todoReducer, initialState);
 	return (
 		<div>
-			<h1>TodoApp</h1>
+			<h1>TodoApp ({todos.length})</h1>
 			<hr />
 
-			<ul>
-				<li>Hola</li>
-				<li>Mundo</li>
-				<li>Brandon</li>
-			</ul>
+			<div className='row'>
+				<div className='col-7'>
+					<ul className='list-group list-group-flush'>
+						{todos.map((todo, i) => (
+							<li className='list-group-item' key={todo.id}>
+								<p className='text-center'>
+									{i + 1}. {todo.desc}
+								</p>
+								<button type='button' class='btn btn-danger'>
+									Borras
+								</button>
+							</li>
+						))}
+					</ul>
+				</div>
+				<div className='col-5'>
+					<h4>Agregar TODO</h4>
+					<hr />
+
+					<form action=''>
+						<input
+							type='text'
+							name='description'
+							placeholder='Aprender...'
+							autoComplete='off'
+							className='form-control'
+						/>
+						<button type='button' className='btn btn-info mt-1 btn-block'>
+							Agregar
+						</button>
+					</form>
+				</div>
+			</div>
 		</div>
 	);
 };
