dd9a32a47c294abff428e6386b06d179e4fb383c
BrandonDevU95 <brandon.vargas1095@hotmail.com>
2020-10-15 22:58:08 -0500

    CreateContext and useContext Video 140

--- 05-Hook-App/src/Components/09-useContext/HomeScreen.jsx
+++ 05-Hook-App/src/Components/09-useContext/HomeScreen.jsx
@@ -1,6 +1,8 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { UserContext } from './UserContext';
 
 export const HomeScreen = () => {
+	const userContext = useContext(UserContext);
 	return (
 		<div>
 			<h1>HomeScreen</h1>
--- 05-Hook-App/src/Components/09-useContext/MainApp.jsx
+++ 05-Hook-App/src/Components/09-useContext/MainApp.jsx
@@ -1,6 +1,16 @@
 import React from 'react';
 import { AppRouter } from './AppRouter';
+import { UserContext } from './UserContext';
 
 export const MainApp = () => {
-	return <AppRouter />;
+	const user = {
+		id: 1345,
+		name: 'Brandon',
+		email: 'brandon.v@gmail.com',
+	};
+	return (
+		<UserContext.Provider value={user}>
+			<AppRouter />
+		</UserContext.Provider>
+	);
 };
