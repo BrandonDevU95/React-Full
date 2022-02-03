3c6f74698901e753c352022d8e84f881ffe9a85c
BrandonDevU95 <brandon.vargas1095@hotmail.com>
2022-02-03 14:24:27 -0600

    175. Navigate push / replace - useNavigate

--- 07-Heroes-app/src/Components/UI/Navbar.jsx
+++ 07-Heroes-app/src/Components/UI/Navbar.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
-import { Link, NavLink } from 'react-router-dom';
+import { Link, NavLink, useNavigate } from 'react-router-dom';
 
 export const Navbar = () => {
+   const navigate = useNavigate();
+
    const handleLogout = () => {
-      console.log('Logout..');
+      navigate('/login', { replace: true });
    };
 
    return (
