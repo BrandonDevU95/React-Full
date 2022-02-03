e60f1a735c4bf93328cf01853bace6db0f3cc2ce
BrandonDevU95 <brandon.vargas1095@hotmail.com>
2022-02-03 15:04:58 -0600

    177. Tarjetas con la información del Héroe

--- 07-Heroes-app/src/Components/Hero/HeroList.jsx
+++ 07-Heroes-app/src/Components/Hero/HeroList.jsx
@@ -1,15 +1,14 @@
+import { HeroCard } from './HeroCard';
 import { getHeroesByPublisher } from '../../Selectors/getHeroesByPublisher';
 
 export const HeroList = ({ publisher }) => {
    const heroes = getHeroesByPublisher(publisher);
 
    return (
-      <>
-         <ul>
-            {heroes.map((hero) => (
-               <li key={hero.id}>{hero.superhero}</li>
-            ))}
-         </ul>
-      </>
+      <div className="row rows-cols-1 row-cols-md-3 g-3">
+         {heroes.map((hero) => (
+            <HeroCard key={hero.id} {...hero} />
+         ))}
+      </div>
    );
 };
