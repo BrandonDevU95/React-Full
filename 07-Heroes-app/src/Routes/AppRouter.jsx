8a28448604ebee99e6e085026f0aab687b63fa2a
BrandonDevU95 <brandon.vargas1095@hotmail.com>
2022-02-06 00:31:56 -0600

    195. Rutas privadas

--- 07-Heroes-app/src/Routes/AppRouter.jsx
+++ 07-Heroes-app/src/Routes/AppRouter.jsx
@@ -1,14 +1,23 @@
-import React from 'react';
-import { Route, Routes, BrowserRouter } from 'react-router-dom';
-import { LoginScreen } from '../Components/Login/LoginScree';
-import { DashboardRoutes } from './DashboardRoutes';
+import React from "react";
+import { Route, Routes, BrowserRouter } from "react-router-dom";
+import { LoginScreen } from "../Components/Login/LoginScree";
+import { DashboardRoutes } from "./DashboardRoutes";
+import { PrivateRoter } from "./PrivateRoter";
 
 export const AppRouter = () => {
    return (
       <BrowserRouter>
          <Routes>
             <Route path="/login" element={<LoginScreen />} />
-            <Route path="/*" element={<DashboardRoutes />} />
+            <Route
+               path="/*"
+               element={
+                  <PrivateRoter>
+                     <DashboardRoutes />
+                  </PrivateRoter>
+               }
+            />
+            {/* <Route path="/*" element={<DashboardRoutes />} /> */}
          </Routes>
       </BrowserRouter>
    );
