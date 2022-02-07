16165bf4d6ccbcc049dec7d0e6cbd650c622cf89
BrandonDevU95 <brandon.vargas1095@hotmail.com>
2022-02-07 02:51:33 -0600

    204. Pruebas AppRouter - Usuario autenticado

--- 07-Heroes-app/src/Test/Routers/AppRouter.test.js
+++ 07-Heroes-app/src/Test/Routers/AppRouter.test.js
@@ -3,13 +3,13 @@ import { AuthContext } from "../../Auth/authContext";
 import { mount } from "enzyme";
 
 describe("Pruebas en AppRouter", () => {
-   const contextValue = {
-      user: {
-         logged: false,
-      },
-   };
-
    test("debe de mostrar login si no esta autenticado", () => {
+      const contextValue = {
+         user: {
+            logged: false,
+         },
+      };
+
       const wrapper = mount(
          <AuthContext.Provider value={contextValue}>
             <AppRouter />
@@ -18,5 +18,23 @@ describe("Pruebas en AppRouter", () => {
 
       expect(wrapper).toMatchSnapshot();
       expect(wrapper.find("h1").text().trim()).toBe("LoginScreen");
+   });
+
+   test("debe de mostrar el componente de marvel si esta autenticado", () => {
+      const contextValue = {
+         user: {
+            logged: true,
+            name: "Juan",
+         },
+      };
+
+      const wrapper = mount(
+         <AuthContext.Provider value={contextValue}>
+            <AppRouter />
+         </AuthContext.Provider>
+      );
+
+      expect(wrapper).toMatchSnapshot();
+      expect(wrapper.find(".navbar").exists()).toBe(true);
    });
 });
