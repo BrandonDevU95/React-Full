ca6380e52eac43939204b8f1face775e47752050
BrandonDevU95 <brandon.vargas1095@hotmail.com>
2022-02-07 22:45:22 -0600

    236. Estructura y diseño del HomeScreen

--- 08-Journal-app/src/components/notes/NoteScreen.jsx
+++ 08-Journal-app/src/components/notes/NoteScreen.jsx
@@ -4,6 +4,27 @@ export const NoteScreen = () => {
    return (
       <div className="notes__main-content">
          <NotesAppBar />
+         <div className="notes__content">
+            <form className="notes__main-content">
+               <input
+                  type="text"
+                  placeholder="Some awesome title"
+                  className="notes__title-input"
+                  autoComplete="off"
+               />
+               <textarea
+                  placeholder="What happened today?"
+                  className="notes__textarea"
+               />
+
+               <div className="notes__images">
+                  <img
+                     src="https://i.picsum.photos/id/278/400/400.jpg?hmac=t2xECpUJ_9FfDdsDq-JUjo6swqIRTPO7jd4sRmZSmDE"
+                     alt="Someone"
+                  />
+               </div>
+            </form>
+         </div>
       </div>
    );
 };
